6ef7de75868ce902c6f0f3e6b6f171cde386b2ba
daolao <dalao1002@gmail.com>
2018-09-27 00:18:21 +0200

    textfield para introducir la edad

--- app/src/main/java/ps/grupo/once/spaceinvaders/view/MainActivity.java
+++ app/src/main/java/ps/grupo/once/spaceinvaders/view/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import ps.grupo.once.spaceinvaders.R;
 import ps.grupo.once.spaceinvaders.SpaceInvadersActivity;
 
@@ -15,22 +16,35 @@ public class MainActivity extends Activity {
 
         setContentView(R.layout.select_age);
 
-        final Button adultButton = (Button) this.findViewById(R.id.adult);
-        adultButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Log.d("debug", "selected adult");
-                Intent intent = new Intent(MainActivity.this, SpaceInvadersActivity.class);
-                intent.putExtra("adult", true);
-                startActivity(intent);
-            }
-        });
+//        final Button adultButton = (Button) this.findViewById(R.id.adult);
+//        adultButton.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                Log.d("debug", "selected adult");
+//                Intent intent = new Intent(MainActivity.this, SpaceInvadersActivity.class);
+//                intent.putExtra("adult", true);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        final Button babyButton = (Button) this.findViewById(R.id.baby);
+//        babyButton.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                Log.d("debug", "selected baby");
+//                Intent intent = new Intent(MainActivity.this, SpaceInvadersActivity.class);
+//                intent.putExtra("adult", false);
+//                startActivity(intent);
+//            }
+//        });
 
-        final Button babyButton = (Button) this.findViewById(R.id.baby);
-        babyButton.setOnClickListener(new View.OnClickListener() {
+        final EditText text = (EditText) this.findViewById(R.id.age);
+
+        final Button confirm = (Button) this.findViewById(R.id.confirm);
+        confirm.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Log.d("debug", "selected baby");
+                String str = text.getText().toString();
+                Log.d("debug", str);
                 Intent intent = new Intent(MainActivity.this, SpaceInvadersActivity.class);
-                intent.putExtra("adult", false);
+                intent.putExtra("adult", Integer.parseInt(str) >= 16);
                 startActivity(intent);
             }
         });
